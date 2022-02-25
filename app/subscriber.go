647057c374dc188bf4c619d440de6da452ff4ef5
Miyagawa Ryohei <ryohei06143510@gmail.com>
2022-02-25 13:55:55 +0900

    delete duplicate message

--- app/subscriber.go
+++ app/subscriber.go
@@ -47,6 +47,10 @@ func (s *Subscriber) Listen(pollingSize int) {
 			exist := false
 			for _, m2 := range msgs {
 				if m.GetChangeVisibilityID() == m2.GetChangeVisibilityID() {
+					if err := queue.DeleteMessage(m); err != nil {
+						s.log.Error("duplicate message cannot delete %s", m.GetDeleteID())
+						continue
+					}
 					exist = true
 				}
 			}
